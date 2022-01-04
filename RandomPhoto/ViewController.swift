3c3fcaf440b2713312a62a89bd4c59ff13613394
rahqis <rahqi@ad.unc.edu>
2022-01-03 22:23:25 -0500

    Initial Commit

---
+++ RandomPhoto/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomPhoto
+//
+//  Created by Rahqi T. Sarsour on 1/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
